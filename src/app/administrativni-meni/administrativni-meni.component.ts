f8b0977917785471213d223a656f0bae6a4b3866
Igor Ratković <iratkovic@smart.edu.rs>
2021-06-17 16:45:53 +0200

    kraj 100%

--- src/app/administrativni-meni/administrativni-meni.component.ts
+++ src/app/administrativni-meni/administrativni-meni.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Dish } from '../dish';
 import { deteleDishByID, Plate } from '../Plate';
 
@@ -10,26 +11,28 @@ import { deteleDishByID, Plate } from '../Plate';
 })
 export class AdministrativniMeniComponent implements OnInit {
   constructor() { }
-  dishes = Plate;
-  filteredDishes = this.dishes;
+  dishes:any[];
+  filteredDishes:any[];
   admpretraga='';
   ovo: any;
   ngOnInit(): void {
+    this.dishes = Plate;
   }
 
   OnClickObrisi(dish:any): void{
     deteleDishByID(dish.id);
-    this.dishes=Plate;
+    this.filteredDishes=Plate;
     
   }
 
   Autopretraga() {
     this.filteredDishes = [];
+    this.dishes = Plate;
     this.dishes.forEach((elem, idx) => {
       if (elem.name.toLowerCase().includes(this.admpretraga.toLowerCase())) {
         this.filteredDishes.push(elem);
       }
-    })
+    });
   }
 }
 
--- src/app/dish-list/disl-list.component.ts
+++ src/app/dish-list/disl-list.component.ts
@@ -8,13 +8,14 @@ import { Plate } from '../Plate';
   styleUrls: ['./disl-list.component.css']
 })
 export class DishListComponent implements OnInit {
-  dishes = Plate;
-  filteredDishes = this.dishes;
+  dishes:any[];
+  filteredDishes:any[];
   selectedDish?: Dish;
   dlpretraga='';
   constructor() { }
 
   ngOnInit(): void {
+    this.dishes = Plate;
     
   }
 
@@ -28,11 +29,13 @@ export class DishListComponent implements OnInit {
 
   Autopretraga() {
     this.filteredDishes = [];
+    this.dishes = Plate;
+    console.log(Plate);
     this.dishes.forEach((elem, idx) => {
       if (elem.name.toLowerCase().includes(this.dlpretraga.toLowerCase())) {
         this.filteredDishes.push(elem);
       }
-    })
+    });
   }
 
 }
